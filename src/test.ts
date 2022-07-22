fe8bf3af2ccaac72ff7bd05ff8984b4626d034fc
MarwanMohamed02 <marwanabbas2002@yahoo.com>
2022-07-22 14:23:38 +0200

    Commiting from vs code

--- src/test.ts
+++ src/test.ts
@@ -10,32 +10,6 @@ interface Data {
 
 }
 
-// let url = "http://api.weatherstack.com/current?access_key=5ddff8cbfe49cb30e6171de9efb0d984&query=";
-
-// request({ url: url,json: true }, (error: Error, response:request.Response) => {
-//     if (error) {
-//         console.log("Unable to connect to weather services!");
-//     }
-//     else if (response.body.error) {
-//         console.log("Unable to find location");
-//     }
-//     else
-//         console.log(response.body.current.weather_descriptions[0] + ", Temperature is currently " + response.body.current.temperature + ". It feels like " + response.body.current.feelslike);
-
-// } )
-
-
-// let geoCode = "https://api.mapbox.com/geocoding/v5/mapbox.places/2ess.json?access_token=pk.eyJ1IjoibWFyd2FuMDIiLCJhIjoiY2w1YmNvdmxqMDY5eDNlb2U2NWkzcTA2cCJ9.plf8TnzZsNx3Dk_nNJQhlA&limit=1";
-
-// request ({url:geoCode, json: true}, (error:Error, response:request.Response) => {
-//     if (error)
-//         console.log("Unable to connect to loction services!");
-//     else if (response.body.features.length === 0)
-//         console.log("Unable to find location... Try again with different search term");
-//     else
-//         console.log(response.body.features[0].center[1], response.body.features[0].center[0]);
-// })
-
 
 function getRequest(URL: string): Promise<any> {
     return new Promise((resolve, reject) => {
